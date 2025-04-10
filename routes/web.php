5d6367f88e775f674f6b3b62cb79042d4edb73c5
ReservaDoPretti <reservadorpretti@outlook.com>
2025-04-10 15:22:23 -0300

    Cliente Show

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('clientes')->group(function () {
     Route::get('/', \App\Livewire\Clientes\Index::class)->name('clientes.index');
     Route::get('/create', \App\Livewire\Clientes\Create::class)->name('clientes.create');
-    Route::get('/{cliente}', \App\Livewire\Clientes\Show::class)->name('clientes.show');
+    Route::get('/{cliente}', \App\Livewire\Clientes\Show::class)->name('clientes.show');  
     Route::get('/{cliente}/edit', \App\Livewire\Clientes\Edit::class)->name('clientes.edit');
 });
 
